570494c79d561fcc5ea34c8ce66ff43b888eda94
danielancines <daniel.ancines@gmail.com>
2018-08-21 14:18:54 -0300

    Module Finished

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -11,14 +11,11 @@ import { HomeComponent } from './home/home.component';
 import { routes } from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
 import { OrderSummaryComponent } from './order/order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -40,14 +37,11 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
     ReactiveFormsModule,
-    SharedModule
+    SharedModule.forRoot()
   ],
   providers: [
-    RestaurantsService,
-    ShoppingCartService,
-    OrderService,
     { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
--- meat-app-starter/src/app/shared/shared.module.ts
+++ meat-app-starter/src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './input/input.component';
 import { RadioComponent } from './radio/radio.component';
 import { RatingComponent } from './rating/rating.component';
+import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { OrderService } from '../order/order.service';
 
 @NgModule({
   imports: [
@@ -26,4 +29,11 @@ import { RatingComponent } from './rating/rating.component';
   ]
 })
 
-export class SharedModule { }
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantsService, OrderService]
+    };
+  }
+}
